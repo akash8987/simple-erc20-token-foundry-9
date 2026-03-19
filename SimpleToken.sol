c5e4e6cf4f6505147596f1c055800877454881e3
Akash <161524961+akash8987@users.noreply.github.com>
2026-03-19 21:34:02 +0600

    Create SimpleToken.sol

---
+++ SimpleToken.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title SimpleToken
+ * @dev Implementation of a basic ERC20 Token with Minting capabilities.
+ */
+contract SimpleToken is ERC20, Ownable {
+    constructor(string memory name, string memory symbol, uint256 initialSupply) 
+        ERC20(name, symbol) 
+        Ownable(msg.sender) 
+    {
+        _mint(msg.sender, initialSupply * 10 ** decimals());
+    }
+
+    /**
+     * @dev Function to mint new tokens. Only the owner can call this.
+     * @param to The address that will receive the minted tokens.
+     * @param amount The amount of tokens to mint.
+     */
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
